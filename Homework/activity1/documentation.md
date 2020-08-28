f75ebbdbc22d6c8f41140335519e32445509ce57
LouLouBadillo <lulu.badillo.csm@gmail.com>
2020-08-27 20:18:42 -0500

    add documentation

---
+++ Homework/activity1/documentation.md
@@ -0,0 +1,24 @@
+# Documentación 
+
+## Complejidad del Algoritmo
+1. sumaIterativa: O(n)
+2. sumaRecursiva: O(n)
+3. sumaDirecta: O(1)
+
+## Plan de Pruebas
+1. Número entero positivo
+   - Entrada: n = 100
+   - Salida esperada: 5050
+   - Resultado: Funciona para los 3 métodos
+2. Entrada 1
+   - Entrada: n = 1
+   - Salida esperada: 1
+   - Resultado: Funciona para los 3 métodos
+3. Entrada 0
+   - Entrada: n = 0
+   - Salida esperada: 0
+   - Resultado: Funciona para los 3 métodos
+4. Número entero negativo 
+   - Entrada: n = -2
+   - Salida esperada: Mensaje de error y regresar -1 
+   - Resultado: Funciona para los 3 métodos
--- Homework/activity1/sum.cpp
+++ Homework/activity1/sum.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int sumaIterativa(int n){
-    int suma; 
-    for(int i = n; i>0; i--){
+    int suma = 0; 
+    if (n < 0){
+        cout << "Error: Número negativo" << endl; 
+        return -1; 
+    }
+    for(int i = 1; i<=n; i++){
         suma += i; 
     }
     return suma; 
@@ -11,19 +15,45 @@ int sumaIterativa(int n){
 
 int sumaRecursiva(int n){
     if (n < 0){
+        cout << "Error: Número negativo" << endl; 
+        return -1; 
+    }
+    else if( n == 0){
         return 0; 
     }
     return n + sumaRecursiva(n-1); 
 }
 
 int sumaDirecta(int n){
-    return (n/2)*(1+n); 
+    if (n < 0){
+        cout << "Error: Número negativo" << endl; 
+        return -1; 
+    } 
+    return (0.5)*n*(1+n); 
 }
 
 int main(){
-    int example = 100; 
-    cout << "Suma Iterativa: " << sumaIterativa(example) << endl; 
-    cout << "Suma Recursiva: " << sumaRecursiva(example) << endl; 
-    cout << "Suma Recursiva: " << sumaDirecta(example) << endl;
+
+    // Pruebas
+    int example1 = 100; 
+    int example2 = 1; 
+    int example3 = 0; 
+    int example4 = -2; 
+    cout << "Prueba 1: n = 100" << endl; 
+    cout << "Suma Iterativa: " << sumaIterativa(example1) << endl; 
+    cout << "Suma Recursiva: " << sumaRecursiva(example1) << endl; 
+    cout << "Suma Directa: " << sumaDirecta(example1) << endl;
+    cout << "Prueba 2: n = 1" << endl;
+    cout << "Suma Iterativa: " << sumaIterativa(example2) << endl; 
+    cout << "Suma Recursiva: " << sumaRecursiva(example2) << endl; 
+    cout << "Suma Directa: " << sumaDirecta(example2) << endl;
+    cout << "Prueba 3: n = 0" << endl;
+    cout << "Suma Iterativa: " << sumaIterativa(example3) << endl; 
+    cout << "Suma Recursiva: " << sumaRecursiva(example3) << endl; 
+    cout << "Suma Directa: " << sumaDirecta(example3) << endl;
+    cout << "Prueba 4: n = -2" << endl;
+    cout << "Suma Iterativa: " << sumaIterativa(example4) << endl; 
+    cout << "Suma Recursiva: " << sumaRecursiva(example4) << endl; 
+    cout << "Suma Directa: " << sumaDirecta(example4) << endl;
     return 0; 
 }
